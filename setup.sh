fbcfff4468829be58ef6d387becaca5a9e8695cb
danihek07 <danihek07@gmail.com>
2024-07-28 21:10:52 +0000

    first commit

---
+++ setup.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# clone repo and save path as variable
+git clone https://github.com/jackyzha0/quartz
+cd quartz
+path="$(pwd)"
+echo "[INFO]: PATH: $path"
+
+# npm
+npm i
+
+# creating starting script in /usr/bin/
+ cd ..
+echo "[INFO]: Creating starting script..."
+echo "#!/bin/bash" > quartz-start.sh
+echo "cd $path" >> quartz-start.sh
+echo "npx quartz build --serve" >> quartz-start.sh
+sudo chmod +x quartz-start.sh
+sudo cp quartz-start.sh /usr/bin/
+
+# install systemd service
+cd ..
+echo "[INFO]: Installing systemd service..."
+sudo cp quartz-server.service /etc/systemd/system/
+sudo systemctl enable quartz-server.service
+sudo systemctl start quartz-server.service
---
+++ uninstall.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "[INFO]: Uninstalling yt-local..."
+sudo systemctl stop quartz-server.service
+sudo systemctl disable quartz-server.service
+sudo rm /etc/systemd/system/quartz-server.service
+sudo systemctl daemon-reload
+sudo rm -rf quartz
